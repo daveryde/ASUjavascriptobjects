a14290091abfee0ec39bc9a00dc2a0909842e969
daveryde <d.mcordova87@gmail.com>
2020-03-31 19:41:44 -0700

    added checkbox functionality and code refactor

--- objects.js
+++ objects.js
@@ -1,24 +1,33 @@
 // Global Scope Variables
 var fnameInput, lnameInput, mostFavInput, leastFavInput;
-var fname, lname, mostFav, leastFav;
+var checkboxOne, checkboxTwo, checkboxThree, checkboxFour, checkboxFive;
+var checkboxes = [];
 
 // Custom Object Constructor
-function CharacterChoice(firstName, lastName, mostFavorite, leastFavorite) {
+function CharacterChoice(
+  firstName,
+  lastName,
+  mostFavorite,
+  leastFavorite,
+  checkboxes = []
+) {
   this.firstName = firstName,
   this.lastName = lastName,
   this.mostFavorite = mostFavorite,
-  this.leastFavorite = leastFavorite
+  this.leastFavorite = leastFavorite,
+  this.checkboxes = checkboxes;
 }
 
+// Entry Object Handler
 function createEntry(newObj) {
   // Grab the table body element
   var tableBody = document.querySelector('#table-body');
 
   // Create inner table elements with object data
-  var tableData = document.createElement("tr")
+  var tableData = document.createElement('tr');
 
   // Loop through the object and append values to table data
-  for(item in newObj) {
+  for (item in newObj) {
     tableData.innerHTML += '<td>' + newObj[item] + '</td>';
   }
 
@@ -26,6 +35,7 @@ function createEntry(newObj) {
   tableBody.appendChild(tableData);
 }
 
+// DOM Element Handler
 function getElements() {
   // Find the input elements and set them to global variables
   fnameInput = document.querySelector('#fname');
@@ -33,11 +43,55 @@ function getElements() {
   mostFavInput = document.querySelector('#top-character');
   leastFavInput = document.querySelector('#bottom-character');
 
-  // Assign input values to global variables for conciseness
-  fname = fnameInput.value;
-  lname = lnameInput.value;
-  mostFav = mostFavInput.value;
-  leastFav = leastFavInput.value;
+  // Find the select elements and set them to global variables
+  checkboxOne = document.querySelector('#checkbox-one');
+  checkboxTwo = document.querySelector('#checkbox-two');
+  checkboxThree = document.querySelector('#checkbox-three');
+  checkboxFour = document.querySelector('#checkbox-four');
+  checkboxFive = document.querySelector('#checkbox-five');
+}
+
+// Checkbox value handler
+function getCheckboxes() {
+  var tempArr = [
+    checkboxOne,
+    checkboxTwo,
+    checkboxThree,
+    checkboxFour,
+    checkboxFive
+  ];
+
+  for (item in tempArr) {
+    if (tempArr[item].checked) {
+      // Temporarily save the value for formatting
+      // Ensure value is a string
+      // Convert the string to lower case then capitalize the first letter
+      var preformat = tempArr[item].value.toString().toLowerCase();
+
+      var postformat = preformat.charAt(0).toUpperCase();
+
+      console.log(postformat);
+
+      // Put the checked and formatted value in our global temporary variabble array of checkedbox values
+      checkboxes.push(postformat);
+    }
+  }
+}
+
+function validateInput() {
+  // Check if the input fields are blank and returns alert if empty
+  if (
+    fnameInput.value === '' ||
+    lnameInput.value === '' ||
+    mostFavInput.value === '' ||
+    leastFavInput.value === ''
+  ) {
+    alert('Please fill in empty fields');
+    return false;
+  }
+
+  // If validated, return true
+  return true;
 }
 
 function clearInputs() {
@@ -46,6 +100,13 @@ function clearInputs() {
   lnameInput.value = '';
   mostFavInput.value = '';
   leastFavInput.value = '';
+
+  // Clear the checkbox choices
+  checkboxOne.checked = false;
+  checkboxTwo.checked = false;
+  checkboxThree.checked = false;
+  checkboxFour.checked = false;
+  checkboxFive.checked = false;
 }
 
 // Form Controller
@@ -56,24 +117,49 @@ function handleForm(e) {
   // Grab the user input element values
   getElements();
 
-  if(fname === '' || lname === '' || mostFav === '' || leastFav === '') {
-    alert('Please fill in empty fields')
-    return false;
-  }
+  // Grab the user checked checkbox values
+  getCheckboxes();
+
+  // var validated = validateInput();
+
+  // if (!validated) {
+  //   return false;
+  // }
 
   // Create an entry object with user input
-  var entryObj = new CharacterChoice(fname, lname, mostFav, leastFav);
+  var entryObj = new CharacterChoice(
+    fnameInput.value,
+    lnameInput.value,
+    mostFavInput.value,
+    leastFavInput.value,
+    checkboxes
+  );
+  // var entryObj = new CharacterChoice(
+  //   fnameInput.value,
+  //   lnameInput.value,
+  //   mostFavInput.value,
+  //   leastFavInput.value,
+  //   checkboxOne.value,
+  //   checkboxTwo.value,
+  //   checkboxThree.value,
+  //   checkboxFour.value,
+  //   checkboxFive.value
+  // );
 
   // Display the object on the document
   createEntry(entryObj);
 
   // Clear input values
   clearInputs();
+
+  console.log(entryObj);
 }
 
 // Adds form submit button click listener
 function createListeners() {
-  document.querySelector('#submit').addEventListener('click', handleForm, false);
+  document
+    .querySelector('#submit')
+    .addEventListener('click', handleForm, false);
 }
 
 window.addEventListener('load', createListeners, false);
